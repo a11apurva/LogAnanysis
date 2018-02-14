92035da86b4db960c5940270558939c1fe05be96
Anubhav Apurva <anubhavh3@gmail.com>
2018-02-14 11:30:24 +0530

    modified buffer to 16*1024*1024

--- copy files/myshutil.py
+++ copy files/myshutil.py
@@ -57,7 +57,7 @@ try:
 except NameError:
     WindowsError = None
 
-def copyfileobj(fsrc, fdst, length=16*1024):
+def copyfileobj(fsrc, fdst, length=16*1024*1024):
     """copy data from file-like object fsrc to file-like object fdst"""
     while 1:
         buf = fsrc.read(length)
