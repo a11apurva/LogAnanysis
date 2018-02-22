e7c774f3a2b19008a2365361fb18f02814974ad1
Anubhav Apurva <anubhavh3@gmail.com>
2018-02-22 17:24:01 +0530

    Add files via upload

--- nlp/senti_2.py
+++ nlp/senti_2.py
@@ -7,14 +7,24 @@ for word in open('negative_1_github.txt','r'):
 
 folder=os.listdir('curie_logs')
 
-g=open('negative_1_github.txt','r')
+
+
+negative_log_words=set()
 
 for file in folder:
+    print "open file : " +file
     for line in open("curie_logs//"+file,'r'):
         for word in nltk.word_tokenize(line.strip()):
             if word in negative_words:
-                g.write(word+"\n")
-                print word
+                negative_log_words.add(word)
+                #print word
+
+
+g=open('negative_log_words.txt','w')
+
+for word in negative_log_words:
+    g.write(word+"\n")
+
 
 g.close()
 
