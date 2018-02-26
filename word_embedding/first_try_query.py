c7bfcdef331e334427a08d89ffb34a07993ef8d3
Anubhav Apurva <anubhavh3@gmail.com>
2018-02-26 16:48:05 +0530

    Add files via upload

--- word_embedding/first_try_query.py
+++ word_embedding/first_try_query.py
@@ -5,7 +5,7 @@ model = gensim.models.doc2vec.Doc2Vec.load('doc2vec.model')
 
 while(1):
     try:
-        inp=input("Enter query: ")
+        inp=input("Enter query: ")          #quotes required for python 2.7
         print (model.most_similar(inp))
     except Exception as e:
         pass
