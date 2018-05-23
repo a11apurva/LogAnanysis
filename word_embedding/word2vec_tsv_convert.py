af5b18b775637047d62d00f85a34aa9aa6b10332
Anubhav Apurva <anubhavh3@gmail.com>
2018-05-23 14:02:17 +0530

    Add files via upload

--- word_embedding/word2vec_tsv_convert.py
+++ word_embedding/word2vec_tsv_convert.py
@@ -1,9 +1,11 @@
 import gensim
 
-file_model="doc2vec.model"
-file_text="converted_model"
+file_model="word2vec_tsv_convert.py"
+file_text="bigfile_converted_model"
 
 model = gensim.models.doc2vec.Doc2Vec.load(file_model)
+#model =gensim.models.Word2Vec.load_word2vec_format(file_model,binary=False)
+#model=gensim.models.KeyedVectors.load_word2vec_format(file_model,binary=False)
 model.save_word2vec_format(file_text+".txt", binary=False)
 
 
